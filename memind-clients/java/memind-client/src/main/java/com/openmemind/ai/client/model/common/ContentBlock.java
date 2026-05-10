a3233703d5deafa86c896f41e66d17cf48340a4b
starboyate <2925776766@qq.com>
2026-05-10 16:53:09 +0800

    feat(client): add exception hierarchy, enums, ContentBlock and Source models

---
+++ memind-clients/java/memind-client/src/main/java/com/openmemind/ai/client/model/common/ContentBlock.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.openmemind.ai.client.model.common;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ContentBlock.TextBlock.class, name = "text"),
+    @JsonSubTypes.Type(value = ContentBlock.ImageBlock.class, name = "image"),
+    @JsonSubTypes.Type(value = ContentBlock.AudioBlock.class, name = "audio"),
+    @JsonSubTypes.Type(value = ContentBlock.VideoBlock.class, name = "video")
+})
+public sealed interface ContentBlock
+        permits ContentBlock.TextBlock,
+                ContentBlock.ImageBlock,
+                ContentBlock.AudioBlock,
+                ContentBlock.VideoBlock {
+
+    record TextBlock(String text) implements ContentBlock {}
+
+    record ImageBlock(Source source) implements ContentBlock {}
+
+    record AudioBlock(Source source) implements ContentBlock {}
+
+    record VideoBlock(Source source) implements ContentBlock {}
+}
---
+++ memind-clients/java/memind-client/src/main/java/com/openmemind/ai/client/model/common/Source.java
@@ -0,0 +1,31 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.openmemind.ai.client.model.common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Source.UrlSource.class, name = "url"),
+    @JsonSubTypes.Type(value = Source.Base64Source.class, name = "base64")
+})
+public sealed interface Source permits Source.UrlSource, Source.Base64Source {
+
+    record UrlSource(String url) implements Source {}
+
+    record Base64Source(@JsonProperty("media_type") String mediaType, String data)
+            implements Source {}
+}
